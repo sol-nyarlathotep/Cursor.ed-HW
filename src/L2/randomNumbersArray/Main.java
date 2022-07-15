e876c1b64475214968ea0154367a5bf114eba89c
rhan1 <thecrapl1945@gmail.com>
2022-07-15 19:14:37 +0300

    Task 1. Add comments.

--- src/L2/randomNumbersArray/Main.java
+++ src/L2/randomNumbersArray/Main.java
@@ -5,14 +5,14 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random randomGenerator = new Random();
-        int[] randomNumbers = new int[10];
-        for (int i = 0; i<randomNumbers.length;i++){
-            while ( true ){
+        Random randomGenerator = new Random();                  // Initialize random number generator class
+        int[] randomNumbers = new int[10];                      // Create an array with 10 elements
+        for (int i = 0; i<randomNumbers.length;i++){            // Traversing an array
+            while ( true ){                                     // Generate number until it even
                 int randomNumber = randomGenerator.nextInt();
                 if (randomNumber % 2 == 0){
-                    randomNumbers[i] = randomNumber;
-                    break;
+                    randomNumbers[i] = randomNumber;            // Write even number to array
+                    break;                                      // Exit from "while"
                 }
             }
         }
