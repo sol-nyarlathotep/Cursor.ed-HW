09f2b2311bb34e542edef6e239edda0e35f88faa
rhan1 <thecrapl1945@gmail.com>
2022-07-15 19:08:56 +0300

    Task 1. Create an array and fill it with random even numbers

--- src/L2/randomNumbersArray/Main.java
+++ src/L2/randomNumbersArray/Main.java
@@ -1,6 +1,21 @@
 package L2.randomNumbersArray;
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random randomGenerator = new Random();
+        int[] randomNumbers = new int[10];
+        for (int i = 0; i<randomNumbers.length;i++){
+            while ( true ){
+                int randomNumber = randomGenerator.nextInt();
+                if (randomNumber % 2 == 0){
+                    randomNumbers[i] = randomNumber;
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(randomNumbers));
     }
 }
